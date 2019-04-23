682ef3790a157c89e3925256c511f25604d7935c
github username <github email address>
2019-04-23 12:33:59 -0700

    Done.

---
+++ Untitled.rb
@@ -0,0 +1,11 @@
+
+
+def get_names(instructors)
+  instructors = [
+  {name: 'Alex', hometown: 'upstate ny', mood: 'excited'},
+  {name: 'rachel', hometown: 'maine'},
+  ‘{name: 'maxwell', hometown: 'brooklyn'}
+  ]
+instructors.map do |name|
+  puts "#{name}"
+End
